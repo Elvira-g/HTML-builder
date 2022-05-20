fde07e5821506f4500d4d70419cee1ea286e7048
Elvira-g <elvira_27@mail.ru>
2022-05-20 19:35:08 +0300

    feat: 03 completed

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'),{withFileTypes: true}, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("\nCurrent directory filenames:");
+      files.forEach(file => {
+          if(!file.isDirectory()) {
+            const link = path.join(__dirname, 'secret-folder', file.name)
+            fs.stat(link, (err, stats) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    const name = path.basename(path.join(__dirname, 'secret-folder', file.name), path.extname(link));
+                    const ext= path.extname(link).split('.');
+                    const extname = ext[1];
+                    const size = stats.size*0.001;
+
+                    console.log(`${name} - ${extname} - ${size}kb`)
+                }
+            })
+          }
+      })
+    }
+})
